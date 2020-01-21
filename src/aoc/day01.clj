79b33728ab5b8b63f04afba082f9d0766f057937
Damien Ragoucy <dam@ramdam.io>
2020-01-21 17:25:24 +0100

    Finish day 01

---
+++ src/aoc/day01.clj
@@ -0,0 +1,22 @@
+(ns aoc.day01
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn part1 [weight]
+  (-> weight (quot 3) (- 2)))
+
+(defn part2 [weight']
+  (loop [weight weight'
+         acc 0]
+    (let [fuel (-> weight (quot 3) (- 2))]
+      (if (> fuel 0)
+        (recur fuel (+ acc fuel))
+        acc))))
+
+(-> (io/resource "day01.txt")
+    slurp
+    (str/split #"\s+")
+    (->> (map #(Integer/parseInt %)))
+    (->> (map part1))
+    ;; (->> (map part2))
+    (->> (apply +)))
