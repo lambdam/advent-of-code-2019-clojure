d4a625b09e0f622ee33363ed20d0fc0148d54940
Damien Ragoucy <dam@ramdam.io>
2020-01-22 12:57:47 +0100

    Add day 02 part 2

--- src/aoc/day02.clj
+++ src/aoc/day02.clj
@@ -2,28 +2,55 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def example "1,9,10,3,2,3,11,0,99,30,40,50")
+(defn instructions-str->vec [string]
+  (-> string
+      str/trim
+      (str/split #",")
+      (->> (mapv #(Integer/parseInt %)))))
 
-(-> (io/resource "day02.txt")
-    slurp
-    #_example
-    str/trim
-    (str/split #",")
-    (->> (mapv #(Integer/parseInt %)))
-    (assoc 1 12)
-    (assoc 2 2)
-    (as-> opcodes'
-      (loop [opcodes opcodes'
-             pointer* 0]
-        (let [v1 (->> pointer* (+ 1) (get opcodes) (get opcodes)) ;; pointer then value
-              v2 (->> pointer* (+ 2) (get opcodes) (get opcodes))
-              dest* (->> pointer* (+ 3) (get opcodes))]
-          (case (get opcodes pointer*)
-            1 (recur (assoc opcodes dest* (+ v1 v2))
-                     (+ 4 pointer*))
-            2 (recur (assoc opcodes dest* (* v1 v2))
-                     (+ 4 pointer*))
-            99 opcodes
-            ;; else
-            nil))))
-    (get 0))
+(defn eval-instructions [{:keys [noun verb instructions]}]
+  (-> instructions
+      (assoc 1 noun)
+      (assoc 2 verb)
+      (as-> opcodes'
+          (loop [opcodes opcodes'
+                 pointer* 0]
+            (let [v1 (->> pointer* (+ 1) (get opcodes) (get opcodes)) ;; pointer then value
+                  v2 (->> pointer* (+ 2) (get opcodes) (get opcodes))
+                  dest* (->> pointer* (+ 3) (get opcodes))]
+              (case (get opcodes pointer*)
+                1 (recur (assoc opcodes dest* (+ v1 v2))
+                         (+ 4 pointer*))
+                2 (recur (assoc opcodes dest* (* v1 v2))
+                         (+ 4 pointer*))
+                99 opcodes
+                ;; else
+                nil))))
+      (get 0)))
+
+(comment ;; part 1
+  (eval-instructions
+    {:noun 12
+     :verb 2
+     :instructions (-> "day02.txt" io/resource slurp instructions-str->vec)})
+  )
+
+(defn look-for-result [{:keys [instructions target]}]
+  (loop [noun 0
+         verb 0]
+    (cond
+      (> noun 99) nil
+      (> verb 99) (recur (inc noun) 0)
+      :else (let [result (eval-instructions {:noun noun
+                                             :verb verb
+                                             :instructions instructions})]
+              (if (= result target)
+                {:noun noun
+                 :verb verb}
+                (recur noun (inc verb)))))))
+
+(comment ;; part 2
+  (look-for-result
+    {:target 19690720
+     :instructions (-> "day02.txt" io/resource slurp instructions-str->vec)})
+  )
